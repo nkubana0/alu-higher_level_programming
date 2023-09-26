3f88a6a6998ef0e691192e385d8b6bc32e912b5a
nkubana0 <i.shema@alustudent.com>
2023-09-26 13:00:51 -0700

    value arguments

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const firstArgs = process.argv.slice(2);
+const secondArgs = process.argv.slice(2);
+if (firstArgs[0] === undefined &&  secondArgs[0] === undefined) {
+  console.log(firstArgs[0] + ' is ' + secondArgs[0]);
+} else {
+  console.log(firstArgs[0] + ' is ' + secondArgs[1]);
+}
