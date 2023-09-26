6afbba3b83607cff35481a657bbf03075fdceb1f
nkubana0 <i.shema@alustudent.com>
2023-09-26 12:35:29 -0700

    value arguments

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 if (args[0] === undefined) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log(args[0]);
+  console.log(args[0]);
 }
