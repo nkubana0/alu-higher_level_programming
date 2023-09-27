da37451f0b0e66ca605798288016811a042d8007
nkubana0 <i.shema@alustudent.com>
2023-09-27 03:46:06 -0700

    to integer

---
+++ javascript-warm_up/5-to_integer.js
@@ -0,0 +1,12 @@
+#!/isr/bin/node
+if (process.argv.length < 3) {
+  console.log('Not a number');
+} else {
+  const firstArgument = process.argv[2];
+  const integerValue = parseInt(firstArgument);
+  if (Number.isInteger(integerValue)) {
+    console.log('My number: ' + integerValue);
+  } else {
+    console.log('My number: ' + integerValue);
+  }
+}
