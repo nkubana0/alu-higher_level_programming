31eb507296f69c8d16292a39207e3fb3323b9044
nkubana0 <i.shema@alustudent.com>
2023-09-28 14:36:44 -0700

    add

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const firstArgument = parseInt(process.argv[2]);
+const secondArgument = parseInt(process.argv[3]);
+if (!isNaN(firstArgument) && !isNaN(secondArgument)) {
+	console.log(add(firstArgument,secondArgument));
+} else {
+	console.log('NaN');
+}
+function add (a,b) {
+	return(a + b);
+}
