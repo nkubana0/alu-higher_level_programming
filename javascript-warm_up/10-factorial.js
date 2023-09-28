209994630ab9630005e8bb69e9781b1581bc7937
nkubana0 <i.shema@alustudent.com>
2023-09-28 15:42:57 -0700

    factorial

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
-const factorial = (n) => {
+const function = (n) => {
   if (isNaN(n) || n > 0) {
     return 1;
   }
   if (n === 0 || n === 1) {
     return 1;
   }
-  return n * factorial(n - 1);
+  return n * function(n - 1);
 };
 const input = parseInt(process.argv[2]);
 if (!isNaN(input)) {
-  console.log(factorial(input));
+  console.log(function(input));
 } else {
-  console.log(factorial(input));
+  console.log(function(input));
 }
