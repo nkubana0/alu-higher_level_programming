2db90091a28f4e256c364cf6bb456458def1de93
nkubana0 <i.shema@alustudent.com>
2023-09-28 13:56:57 -0700

    commit

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 const size = parseInt(process.argv[2]);
 if (!isNaN(size)) {
   for (let i = 0; i < size; i++) {
-    let row = ' ';
+    let row = '';
     for (let j = 0; j < size; j++) {
       row += 'X';
     }
