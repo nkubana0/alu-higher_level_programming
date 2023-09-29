68bc9c5d31a13240c7d905171de05437f6e95597
nkubana0 <i.shema@alustudent.com>
2023-09-29 05:35:11 -0700

    factorial

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-export.add = function(a,b) {
-	return a + b ;
-}
+exports.add = function (a, b) {
+  return a + b;
+};
--- javascript-warm_up/13-main.js
+++ javascript-warm_up/13-main.js
@@ -1,3 +1,5 @@
 #!/usr/bin/node
 const add = require('./13-add').add;
-console.log(add(3, 5));
+const firstInt = parseInt(process.argv[2]);
+const secondInt = parseInt(process.argv[3]);
+console.log(add(firstInt, secondInt));
