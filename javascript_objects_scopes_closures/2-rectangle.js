b066b76b0a934b0b2b0d7af7405568f134c9a8c9
nkubana0 <i.shema@alustudent.com>
2023-09-30 05:48:38 -0700

    rectangle

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
   constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-	    return {};
-    }
-
+    if (w < 0 && h < 0) {
+	    return {}
     this.width = w;
     this.height = h;
+    }
   }
 }
 
