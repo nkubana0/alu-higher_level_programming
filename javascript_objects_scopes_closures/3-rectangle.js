f19b1917dfabe53a4ffffa79c601d3a870248c55
nkubana0 <i.shema@alustudent.com>
2023-09-30 06:43:10 -0700

    rectangle X

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 && h <= 0) {
+      return {};
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let i = 0; i < this.height; i++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Rectangle;
