98b72da5489f5f7e0fad379819bfb238d43e6696
nkubana0 <i.shema@alustudent.com>
2023-10-01 09:58:52 -0700

    factorial

--- javascript-warm_up/102-add_me_maybe.js
+++ javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+
 exports.addMeMaybe = function (number, theFunction) {
   if (number > 0) {
     theFunction(number + 1);
   } else {
-	  theFunction(number + 1);
+    theFunction(number + 1);
   }
 };
