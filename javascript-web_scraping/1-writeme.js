ee46f2c61391c3bba62db4c7af786f58b1e110d7
nkubana0 <i.shema@alustudent.com>
2023-10-05 10:20:19 -0700

    write a text to file

---
+++ javascript-web_scraping/1-writeme.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+const fileText = process.argv[3];
+if (!filePath || !fileText) {
+  console.error('Please add text and a file location');
+  process.exit(1);
+}
+fs.writeFile(filePath, fileText, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Content written to ' + filePath);
+  }
+}
+);
