806513ee1856dd4827797bf609687c564479ba8f
nkubana0 <i.shema@alustudent.com>
2023-10-05 11:25:54 -0700

    write a text to file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,14 +1,18 @@
 #!/usr/bin/node
 
-const fs = require ('fs');
+const fs = require('fs');
 const filePath = process.argv[2];
 const fileText = process.argv[3];
 
-fs.writeText(filePath, fileText, (err) => {
-	if (err) {
-		console.log(err);
-	} else {
-		fs.readFileSync(filePath, 'utf-8');
-	}
+if (!filePath || !fileText) {
+  process.exit(1);
+}
+
+fs.writeFile(filePath, fileText, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    fs.readFileSync(filePath, 'utf-8');
+  }
 }
 );
