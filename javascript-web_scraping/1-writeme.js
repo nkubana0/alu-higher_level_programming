b204a86c63e6fe85937ae5eb9f9c95a08c45d9c2
nkubana0 <i.shema@alustudent.com>
2023-10-05 11:16:51 -0700

    write a text to file

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,19 +1,14 @@
 #!/usr/bin/node
-const fs = require('fs');
 
+const fs = require ('fs');
 const filePath = process.argv[2];
 const fileText = process.argv[3];
 
-if (!filePath || !fileText) {
-  process.exit(1);
+fs.writeText(filePath, fileText, (err) => {
+	if (err) {
+		console.log(err);
+	} else {
+		fs.readFileSync(filePath, 'utf-8');
+	}
 }
-
-function writeText (filePath, fileText) {
-  fs.writeFile(filePath, fileText, 'utf-8', (err) => {
-    if (err) {
-      process.exit(1);
-    }
-  }
-  );
-}
-writeText(filePath, fileText);
+);
