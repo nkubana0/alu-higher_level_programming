2a0a140e15fd2c545dfc4d0bc36d4d72b02be0e6
nkubana0 <i.shema@alustudent.com>
2023-10-05 09:54:45 -0700

    readme script

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+if (!filePath) {
+  console.error('Please provide a file path as an argument');
+  process.exit(1);
+}
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+}
+);
