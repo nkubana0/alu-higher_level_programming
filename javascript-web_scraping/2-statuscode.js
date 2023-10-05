28afea2024653505bdae4c7365b48c15c2e8464d
nkubana0 <i.shema@alustudent.com>
2023-10-05 13:35:55 -0700

    status code

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+
+if (!url) {
+  console.error('Please provide a URL argument');
+  process.exit(1);
+}
+
+request(url, (error, response) => {
+  if (error) {
+    console.error('Error: ', error.message);
+    process.exit(1);
+  }
+  console.log('code: ', response.statusCode);
+});
